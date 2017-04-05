30dbc3d06e022ef8ced11efb2f929d5af1371916
João Pedro Pereira <jplpereira@gmail.com>
2017-04-05 15:37:57 -0300

    Minor fixes in layout

--- ajuda.me/resources/views/courses/index.blade.php
+++ ajuda.me/resources/views/courses/index.blade.php
@@ -9,15 +9,33 @@
 
                 <div class="panel-body">
                     <table align="left">
+                        @if (count($courses) != 0)
+                            <th>ID</th>
+                            <th>Name</th>
+                        @else
+                            <!-- Nothing to show -->
+                        @endif
                         @foreach ($courses as $course)
                             <tr>
-                                <td width="10%">{{ $course->id }}</td>
-                                <td width="50%"><a href="/showcourse/{{$course->id}}">{{ $course->name }}</a></td>
+                                <td width="10%">
+                                    {{ $course->id }}
+                                </td>
+                                <td width="50%">
+                                    <a href="/courses/{{$course->id}}">
+                                        {{ $course->name }}
+                                    </a>
+                                </td>
                             </tr>
                         @endforeach
                     </table>
-
                 </div>
+
+                <div class="panel-body">
+                    <a href="/courses/create">
+                        Create new course
+                    </a>
+                </div>
+
             </div>
         </div>
     </div>
