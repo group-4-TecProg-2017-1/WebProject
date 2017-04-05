f2e5711f283b6a9f5f0f79a612280fc2b97deef7
João Pedro Pereira <jplpereira@gmail.com>
2017-04-05 15:34:55 -0300

    Fixing form inputs layout

--- ajuda.me/resources/views/courses/create.blade.php
+++ ajuda.me/resources/views/courses/create.blade.php
@@ -6,21 +6,22 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Create course</div>
+                <div class="panel-body">
+                    <form class="" action="/courses" method="POST">
+                        {{ csrf_field() }}
 
-                <form class="" action="/courses" method="POST">
-                    {{ csrf_field() }}
+                        <div class="form-group">
+                            <label for="inputId">ID:</label>
+                            <input type="text" name="id" maxlength="6" class="form-control" id="inputId" placeholder="Enter ID">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputName">Name:</label>
+                            <input type="text" name="name" class="form-control" id="inputName" placeholder="Enter name">
+                        </div>
 
-                    <div class="form-group">
-                        <label for="inputId">ID:</label>
-                        <input type="text" name="id" maxlength="6" class="form-control" id="inputId" placeholder="Enter ID">
-                    </div>
-                    <div class="form-group">
-                        <label for="inputName">Name:</label>
-                        <input type="text" name="name" class="form-control" id="inputName" placeholder="Enter name">
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">Create</button>
-                </form>
+                        <button type="submit" class="btn btn-primary">Create</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
