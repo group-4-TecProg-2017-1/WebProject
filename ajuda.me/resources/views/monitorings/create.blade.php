22a23d3bdfd95a9ce327a585e645f4975a711244
João Pedro Pereira <jplpereira@gmail.com>
2017-04-09 22:37:41 -0300

    Fixing Create Monitoring View

--- ajuda.me/resources/views/monitorings/create.blade.php
+++ ajuda.me/resources/views/monitorings/create.blade.php
@@ -7,30 +7,25 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Create monitoring</div>
                 <div class="panel-body">
-                    <form class="" action="/courses" method="POST">
+                    <form class="" action="/monitorings" method="POST">
                         {{ csrf_field() }}
 
                         <div class="form-group">
-                            <label for="inputId">ID:</label>
-                            <input type="text" name="id" maxlength="6" class="form-control" id="inputId" placeholder="Enter ID">
+                            <label for="place">Monitoring Place:</label>
+                            <input type="text" name="place" class="form-control" id="place" placeholder="Enter place">
                         </div>
                         <div class="form-group">
-                            <label for="inputName">Monitoring Place:</label>
-                            <input type="text" name="name" class="form-control" id="inputName" placeholder="Enter name">
+                            <label for="subject">Subject:</label>
+                            <input type="text" name="subject" class="form-control" id="subject" placeholder="Enter subject">
                         </div>
                         <div class="form-group">
-                            <label for="inputName">Content Approached:</label>
-                            <input type="text" name="name" class="form-control" id="inputName" placeholder="Enter name">
+                            <label for="starting_time">Starting time:</label>
+                            <input type="text" name="starting_time" class="form-control" id="starting_time" placeholder="Enter starting time">
                         </div>
                         <div class="form-group">
-                            <label for="inputName">Start Time:</label>
-                            <input type="text" name="name" class="form-control" id="inputName" placeholder="Enter name">
+                            <label for="duration">Duration time:</label>
+                            <input type="text" name="duration" class="form-control" id="duration" placeholder="Enter duration">
                         </div>
-                        <div class="form-group">
-                            <label for="inputName">Duration Time:</label>
-                            <input type="text" name="name" class="form-control" id="inputName" placeholder="Enter name">
-                        </div>
-
 
                         <button type="submit" class="btn btn-primary">Create</button>
                     </form>
