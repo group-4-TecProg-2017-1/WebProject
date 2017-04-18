9b9a0fc502b0926cf88c00e461b19d6f8bdfb1f4
jplpereira <jplpereira@gmail.com>
2017-04-18 01:29:29 -0300

    Creating user edit view

---
+++ ajuda.me/resources/views/users/edit.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Update user</div>
+                <div class="panel-body">
+
+                    <form class="form-horizontal" role="form" method="POST" action="/users/{{ $user->id }}">
+                        {{ csrf_field() }}
+                        {{ method_field('PUT') }}
+                        <div class="form-group">
+                            <label for="name" class="col-md-4 control-label">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ $user->name }}" required autofocus>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ $user->email }}" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="role" class="col-md-4 control-label">Role</label>
+                            <div class="col-md-6">
+                                <input id="role" type="radio"  name="role" value="admin" required autofocus {{ $user->role === 'admin' ? 'checked' : '' }}> Admin <br>
+                                <input id="role" type="radio"  name="role" value="monitor" required autofocus {{ $user->role === 'monitor' ? 'checked' : '' }}> Monitor <br>
+                                <input id="role" type="radio"  name="role" value="student" required autofocus {{ $user->role === 'student' ? 'checked' : '' }}> Student <br>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Update
+                                </button>
+                                <a href="/users" class="btn">
+                                    Cancel
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
