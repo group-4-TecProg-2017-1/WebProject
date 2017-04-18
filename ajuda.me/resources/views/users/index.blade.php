9ed902a5bc33d89137a17dd2aab928c303290ed9
jplpereira <jplpereira@gmail.com>
2017-04-18 01:27:34 -0300

    Including actions' buttons

--- ajuda.me/resources/views/users/index.blade.php
+++ ajuda.me/resources/views/users/index.blade.php
@@ -4,6 +4,12 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+
             <div class="panel panel-default">
                 <div class="panel-heading">Users</div>
 
@@ -13,11 +19,18 @@
                             <th width="20%">Name</th>
                             <th width="40%">Email</th>
                             <th width="20%">Role</th>
+                            <th width="20%" colspan="2">Actions</th>
                             @foreach ($users as $user)
                                 <tr>
                                     <td>{{$user->name}}</td>
                                     <td>{{$user->email}}</td>
                                     <td>{{$user->role}}</td>
+                                    <td>
+                                        <a href="/users/{{$user->id}}/edit" class="btn btn-warning">Update</a>
+                                    </td>
+                                    <td>
+                                        <a href="#" class="btn btn-danger">Delete</a>
+                                    </td>
                                 </tr>
                             @endforeach
                         </table>
@@ -30,7 +43,7 @@
                 <div class="panel-body">
                   <a href="/users/create">Create new user</a>
                 </div>
-                
+
             </div>
         </div>
     </div>
