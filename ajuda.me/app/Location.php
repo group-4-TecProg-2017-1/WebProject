7c9fd4b265ac9800ca037954778705649a6a490a
IagoCarvalho <IagoVC_2012@hotmail.com>
2017-05-07 19:42:06 -0300

    Adding locations id to model

--- ajuda.me/app/Location.php
+++ ajuda.me/app/Location.php
@@ -3,10 +3,20 @@ namespace App;
 
 class Location extends model
 {
+    private $location_id;
     private $description;
     private $building;
     private $room;
 
+
+    public function setLocationID()
+    {
+      $this->location_id = $location_id;
+    }
+    public function getLocationID()
+    {
+      return $this->location_id;
+    }
     public function setLocationDescription()
     {
       $this->description = $description;
