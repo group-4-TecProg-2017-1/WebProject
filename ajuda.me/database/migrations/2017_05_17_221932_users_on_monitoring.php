18abdf56f8eed388e451819ec5a28849106571e5
DESKTOP-6JIHP9J\atillagallio-Alien <atilla.gallio@gmail.com>
2017-05-17 19:25:32 -0300

    creating user on monitoring migration

---
+++ ajuda.me/database/migrations/2017_05_17_221932_users_on_monitoring.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UsersOnMonitoring extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_monitoring', function (Blueprint $table) {
+            $table->integer('id_user')->unsigned();
+            $table->integer('id_monitoring')->unsigned();
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->foreign('id_monitoring')->references('id')->on('monitorings');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_monitoring');
+    }
+}
