366842645f641601105e171ef2d8091e612e2be2
IagoCarvalho <IagoVC_2012@hotmail.com>
2017-05-17 19:15:54 -0300

    Creating monitorings migrations schema

---
+++ ajuda.me/database/migrations/2017_05_17_214629_monitoring.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Monitoring extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('monitorings', function(Blueprint $table){
+          $table->increments('id')->unique();
+          $table->string('contentApproached');
+          $table->string('type');
+          $table->dateTime('startTime');
+          $table->Time('duration');
+          $table->integer('id_location')->unsigned();
+          $table->integer('id_courses')->unsigned();
+          $table->foreign('id_location')->references('id')->on('locations');
+          $table->foreign('id_courses')->references('id')->on('courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('monitorings');
+    }
+}
