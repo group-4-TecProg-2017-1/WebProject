c52f611a9b17c96c7cd2eb71a23997f9d2ef5c3f
DESKTOP-6JIHP9J\atillagallio-Alien <atilla.gallio@gmail.com>
2017-05-18 19:52:51 -0300

    updating monitoring creating form

--- ajuda.me/resources/views/monitorings/create.blade.php
+++ ajuda.me/resources/views/monitorings/create.blade.php
@@ -21,13 +21,51 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="inputBuilding">Start Time</label>
+                            <label for="inputTime">Start Time</label>
                             <input type="datetime-local" name="startTime" class="form-control" id="inputName">
                         </div>
 
                         <div class="form-group">
-                            <label for="inputBuilding">Duration</label>
+                            <label for="inputDuration">Duration</label>
                             <input type="time" name="duration" class="form-control" id="inputName">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="inputDuration">Location</label>
+                            <select class="form-control m-bot15" name="location_id">
+
+                                @if ($locations->count())
+
+                                    @foreach ($locations as $location)
+                                        <option value="{{ $location->id }}" {{ $selectedLocation == $location->id ? 'selected="selected"' : '' }}>{{ $location->description }}</option>
+                                    @endforeach    
+
+                                @endif
+
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="inputDuration">Courses</label>
+                            <select class="form-control m-bot15" name="course_id">
+
+                                @if ($courses->count())
+
+                                    @foreach ($courses as $course)
+                                        <option value="{{ $course->id }}" {{ $selectedCourse == $course->id ? 'selected="selected"' : '' }}>{{ $course->name }}</option>
+                                    @endforeach    
+
+                                @endif
+
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <select multiple="multiple" name="monitors[]" id="monitor_id">
+                                @foreach($monitors as $monitor)
+                                        <option value="{{ $monitor->id }}" {{ $selectedMonitors == $monitor->id ? 'selected="selected"' : '' }}>{{ $monitor->name }}</option>
+                                @endforeach
+                            </select>
                         </div>
 
                         <button type="submit" class="btn btn-primary">Create</button>
