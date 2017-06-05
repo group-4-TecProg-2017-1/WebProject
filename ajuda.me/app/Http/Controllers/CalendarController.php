ab2aba1102e9838c86a0ee4ecada7d5d93ac32d2
DESKTOP-6JIHP9J\atillagallio-Alien <atilla.gallio@gmail.com>
2017-06-04 22:16:06 -0300

    calendar controller

---
+++ ajuda.me/app/Http/Controllers/CalendarController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CalendarController extends Controller
+{
+    /**
+     * Display a listing of the monitors.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        return view('calendar.index');
+    }
+
+    /**
+     * Show the form for creating a new monitor.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Store a newly created monitor in database.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+}
